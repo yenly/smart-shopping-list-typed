c92aa3f54e72f48b02578c249291d7b6c9afd866
Yenly <yencodes@gmail.com>
2021-05-29 15:05:18 -0700

    Text change to test deploy

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Boop! booP! BOOP!
         </a>
       </header>
     </div>
