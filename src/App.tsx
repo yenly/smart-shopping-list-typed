3fc3c725c338e625b784fa67e62e9063323e3e0b
Yenly <yenly@users.noreply.github.com>
2021-05-31 06:57:08 -0700

    Install & configure typescript and antd using craco (#2)
    * Install & configure typescript with craco; convert files and ensure test passes
    
    * Install & configure antd using craco-less
    
    * Remove unused boilerplate files

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.less';
+import { Button } from 'antd';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>This button is not wired up yet.</p>
+        <Button type="primary"> Boop! booP! BOOP!</Button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
