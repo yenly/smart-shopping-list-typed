91840390e4ec6eb182c9f1f53b578773d3d9fc0e
Yenly <yenly@users.noreply.github.com>
2021-07-05 14:54:27 -0700

    Feat Connect firebase (#5)
    * Add firebase dependences
    
    * Connect button to send boop to firestore
    
    * Display boop contents in firestore under button

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,38 @@
 import React from 'react';
 import './App.less';
-import { Button } from 'antd';
+import { BoopFirebase } from './components/BoopFirebase';
+import { useCollection } from 'react-firebase-hooks/firestore';
+import { fb } from './lib/firebase';
 
-function App() {
+const App: React.FC = () => {
+  const db = fb.firestore();
+  const [value, loading, error] = useCollection(db.collection('boops'), {
+    snapshotListenOptions: { includeMetadataChanges: true },
+  });
+  let list;
+  if (value) {
+    list = value.docs.map(doc => {
+      return doc.data();
+    });
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <p>This button is not wired up yet.</p>
-        <Button type="primary"> Boop! booP! BOOP!</Button>
+        <p>Click button to send boops to Firestore</p>
+        <BoopFirebase />
+        {error && <strong>Error: {JSON.stringify(error)}</strong>}
+        {loading && <span>Messages: Loading...</span>}
+        {list && (
+          <ul>
+            {list.map((boop, index) => (
+              <li key={index}>{boop.boop}</li>
+            ))}
+          </ul>
+        )}
       </header>
     </div>
   );
-}
+};
 
 export default App;
